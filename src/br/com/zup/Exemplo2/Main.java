c6e7009b10ca4b13d16c78937621f43f21092048
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-22 15:34:12 -0300

    Exemplo com herois e inimigos comentado

--- src/br/com/zup/Exemplo2/Main.java
+++ src/br/com/zup/Exemplo2/Main.java
@@ -5,16 +5,28 @@ public class Main {
         Guerreiros guerreiro = new Guerreiros("Thor", "Raio", "Armadura", 100, "Martelo");
         Voadores inimigoVoador = new Voadores("Anivia", "Azul", "Gelo", 800, 50);
 
-
+        // Atribuindo o retorno do método na variável danoAplicado
         int danoAplicado = inimigoVoador.aplicarDano();
+
+        // Aplicando dano no guerreiro
         guerreiro.perderVida(danoAplicado);
 
+        // Exibe a vida do inimigo
         System.out.println(inimigoVoador.getVida());
 
+        // Atribuindo o retorno do método na variável danoDoGuerreiro
         int danoDoGuerreiro = guerreiro.aplicaDano();
+
+        // Aplicando dano no inimigo
         inimigoVoador.perderVida(danoDoGuerreiro);
+
+        // Exibe a vida do inimigo
         System.out.println(inimigoVoador.getVida());
+
+        // Aplicando dano no inimigo
         inimigoVoador.perderVida(danoDoGuerreiro);
+
+        // Exibe a vida do inimigo
         System.out.println(inimigoVoador.getVida());
 
 
